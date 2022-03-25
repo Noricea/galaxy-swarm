3e99c39ad13331b409f953cdfd10cd401cf7d249
noricia <noricia@norifox>
2022-03-25 19:57:40 +0100

    Window Setup

---
+++ galaxy-swarm/src/Tester/MainGameLoop.java
@@ -0,0 +1,23 @@
+package Tester;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DisplayManager.createDisplay();
+		
+		while(!Display.isCloseRequested()) {
+			
+			
+			DisplayManager.updateDisplay();
+		}
+		
+		DisplayManager.closeDisplay();
+	}
+
+}
