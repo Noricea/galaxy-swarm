1e7ed4e09561c663c7fbe5feefda962bdda0388f
noricia <noricia@norifox>
2022-03-25 10:40:23 +0100

    initial setup

--- galaxy-swarm/src/bootLoader.java
+++ galaxy-swarm/src/bootLoader.java
@@ -1,8 +1,17 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
 
 public class bootLoader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			Display.create();
+			Thread.sleep(5000);
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
