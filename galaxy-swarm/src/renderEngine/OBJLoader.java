acb2e3a978da37ffc5bfa4bd5ab2653885d6a71b
noricia <noricia@norifox>
2022-03-27 17:02:55 +0200

    load Models

--- galaxy-swarm/src/renderEngine/OBJLoader.java
+++ galaxy-swarm/src/renderEngine/OBJLoader.java
@@ -24,7 +24,7 @@ public class OBJLoader {
 
 		BufferedReader reader = new BufferedReader(fr);
 		String line;
-		List<Vector3f> verticles = new ArrayList<Vector3f>();
+		List<Vector3f> vertices = new ArrayList<Vector3f>();
 		List<Vector2f> textures = new ArrayList<Vector2f>();
 		List<Vector3f> normals = new ArrayList<Vector3f>();
 		List<Integer> indices = new ArrayList<Integer>();
@@ -40,10 +40,10 @@ public class OBJLoader {
 				line = reader.readLine();
 				String[] currentLine = line.split(" ");
 				if (line.startsWith("v ")) {
-					// input for verticles
+					// input for vertices
 					Vector3f vertex = new Vector3f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]),
 							Float.parseFloat(currentLine[3]));
-					verticles.add(vertex);
+					vertices.add(vertex);
 				} else if (line.startsWith("vt ")) {
 					// input for textures
 					Vector2f vertex = new Vector2f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]));
@@ -54,16 +54,66 @@ public class OBJLoader {
 							Float.parseFloat(currentLine[3]));
 					normals.add(vertex);
 				} else if (line.startsWith("f ")) {
-					texturesArray = new float[verticles.size() * 2];
-					normalsArray = new float[verticles.size() * 3];
+					texturesArray = new float[vertices.size() * 2];
+					normalsArray = new float[vertices.size() * 3];
+					break;
 				}
 			}
+
+			while (line != null) {
+				if (!line.startsWith("f ")) {
+					line = reader.readLine();
+					continue;
+				}
+				String[] currentLine = line.split(" ");
+				String[] vertex1 = currentLine[1].split("/");
+				String[] vertex2 = currentLine[2].split("/");
+				String[] vertex3 = currentLine[3].split("/");
+
+				processVertex(vertex1, indices, textures, normals, texturesArray, normalsArray);
+				processVertex(vertex2, indices, textures, normals, texturesArray, normalsArray);
+				processVertex(vertex3, indices, textures, normals, texturesArray, normalsArray);
+
+				line = reader.readLine();
+			}
+			reader.close();
 
 		} catch (Exception e) {
 
 		}
 
-		return null;
+		verticlesArray = new float[vertices.size() * 3];
+		indicesArray = new int[indices.size()];
+
+		int vertexPointer = 0;
+
+		for (Vector3f vertex : vertices) {
+			verticlesArray[vertexPointer++] = vertex.x;
+			verticlesArray[vertexPointer++] = vertex.y;
+			verticlesArray[vertexPointer++] = vertex.z;
+		}
+		
+		for(int i = 0; i < indices.size(); i++) {
+			indicesArray[i] = indices.get(i);
+		}
+
+		return loader.loadToVAO(verticlesArray, texturesArray, indicesArray);
+
+	}
+
+	private static void processVertex(String[] vertexData, List<Integer> indices, List<Vector2f> textures,
+			List<Vector3f> normals, float[] textureArray, float[] normalsArray) {
+
+		int currentVertexPointer = Integer.parseInt(vertexData[0]) - 1;
+		indices.add(currentVertexPointer);
+		Vector2f currentTex = textures.get(Integer.parseInt(vertexData[1]) - 1);
+		textureArray[currentVertexPointer * 2] = currentTex.x;
+		textureArray[currentVertexPointer * 2 + 1] = 1 - currentTex.y;
+		Vector3f currentNorm = normals.get(Integer.parseInt(vertexData[2]) - 1);
+
+		normalsArray[currentVertexPointer * 3] = currentNorm.x;
+		normalsArray[currentVertexPointer * 3 + 1] = currentNorm.y;
+		normalsArray[currentVertexPointer * 3 + 2] = currentNorm.z;
 
 	}
 }
